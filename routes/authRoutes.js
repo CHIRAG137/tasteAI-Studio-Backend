e03856a16fc94672f8aea020c3a2a46bd56a94e3
CHIRAG137 <chiraggoel138@gmail.com>
2025-08-20 16:49:51 +0530

    Add: Get User Details API

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,5 +5,6 @@ const authController = require("../controllers/authController");
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/google-login", authController.googleLogin);
+router.get("/me", authMiddleware, authController.getUserDetails);
 
 module.exports = router;
